55cb7539b38fea70d64fd8d013e191514bd76157
GiadaImmo <120042391+GiadaImmo@users.noreply.github.com>
2023-03-09 13:13:22 +0100

    Lezione_08-03

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
 /*
-A partire dal codice realizzato nell’esercizio precedente realizzate una classe "libro" con un metodo di controllo di una sequenza passata come parametro.
-Poi una classe "archivio" che contiene un array di oggetti di tipo "libro" con un metodo che restituisce un array contenente gli indici dei libri corrispondenti ad una stringa, e un getter per ottenere un libro dato l’indice nell’array.
+A partire dal codice realizzato nell’esercizio precedente realizzate
+una classe "archivio" contenente un metodo che restituisce un
+array di libri che corrispondono ad una certa stringa.
+L’array in cui eseguire la ricerca è caricato in una variabile nella
+stessa classe tramite il costruttore.
+Il singolo libro può essere a sua volta rappresentato da una classe
+Libro con i quattro attributi necessari, specificati nella consegna
+del progetto.
 */
 
 import './style.css';
 
+const libriTrovati = []; // Array che conterrà i libri corrispondenti alla stringa inserita
 
+// Classe contenente il costruttore di un singolo libro e la funzione che riempie l'array libriTrovati
+class Archivio {
+  constructor(titolo, autore) {
+    this.titolo = titolo;
+    this.autore = autore;
+  }
 
+  trovaLibri(stringaInserita) {
+    if (
+      this.autore.toLowerCase().includes(stringaInserita.toLowerCase()) ||
+      this.titolo.toLowerCase().includes(stringaInserita.toLowerCase())
+    ) {
+      libriTrovati.push({ titolo: this.titolo, autore: this.autore });
+    }
+  }
+}
+
+const libri = []; // Lista di libri creata con la classe Archivio
+let stringa = 'os'; // Stringa da cercare
+
+libri[libri.length] = new Archivio('La Divina Commedia', 'Dante Alighieri');
+libri[libri.length] = new Archivio('I Promessi Sposi', 'Alessandro Manzoni');
+libri[libri.length] = new Archivio('Senilità', 'Italo Svevo');
+libri[libri.length] = new Archivio('Una Donna', 'Sibilla Aleramo');
+libri[libri.length] = new Archivio('Harry Potter', 'J.K. Rowling');
+
+libri.forEach((singoloLibro) => singoloLibro.trovaLibri(stringa));
+
+libriTrovati.forEach((singoloLibro) =>
+  console.log(' "' + singoloLibro['titolo'] + '" ' + singoloLibro['autore'])
+);
